10d14b5774391b612e42cc967736f77e48426d0d
Jorge Manrubia <jorge@hey.com>
2025-11-05 08:31:42 +0100

    Don't choke when no collections

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,9 +1,9 @@
 class LandingsController < ApplicationController
   def show
-    if Current.user.collections.many?
-      redirect_to events_path
-    else
+    if Current.user.collections.one?
       redirect_to collection_path(Current.user.collections.first)
+    else
+      redirect_to events_path
     end
   end
 end
--- test/controllers/landings_controller_test.rb
+++ test/controllers/landings_controller_test.rb
@@ -10,6 +10,12 @@ class LandingsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to events_path
   end
 
+  test "redirects to the timeline when no collections" do
+    Collection.destroy_all
+    get landing_path
+    assert_redirected_to events_path
+  end
+
   test "redirects to collections when only one collection" do
     sole_collection, *collections_to_delete = users(:kevin).collections.to_a
     collections_to_delete.each(&:destroy)
