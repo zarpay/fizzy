67b993ede343f2b6e2a7b587b3babc5852a26721
Denis Švara <denis.svara@gmail.com>
2026-01-19 13:51:14 +0100

    Add JSON response support for `notifications/tray`.

--- test/controllers/notifications/trays_controller_test.rb
+++ test/controllers/notifications/trays_controller_test.rb
@@ -11,4 +11,13 @@ class Notifications::TraysControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "div", text: /Layout is broken/
   end
+
+  test "show as JSON" do
+    expected_ids = users(:kevin).notifications.unread.ordered.limit(100).pluck(:id)
+
+    get tray_notifications_path(format: :json)
+
+    assert_response :success
+    assert_equal expected_ids, @response.parsed_body.map { |notification| notification["id"] }
+  end
 end
